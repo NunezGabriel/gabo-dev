506dd70997cfcaba5818c8371b0dda7fa77acec2
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-12-17 12:42:53 -0500

    avatar card 90%

--- components/PresentationSection.tsx
+++ components/PresentationSection.tsx
@@ -4,35 +4,24 @@ import Image from "next/image";
 import Link from "next/link";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import GradientText from "./GradientText";
+import ProfileCard from "../components/ProfileCard";
 
 export default function PresentationSection() {
   return (
     <section className="w-full max-w-6xl mx-auto px-6 py-24">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
-        <div
-          className="relative w-full h-[420px] md:h-[520px] rounded-xl overflow-hidden
-                shadow-[0_30px_80px_-20px_rgba(0,0,0,0.7)]"
-        >
-          <Image
-            src="/avatar.jpg"
-            alt="Avatar"
-            fill
-            className="object-cover"
-            priority
+        <div className="flex justify-center items-center">
+          <ProfileCard
+            name="Gabriel Nunez"
+            title="Software Developer"
+            handle="javicodes"
+            status="Online"
+            avatarUrl="avatar.jpg"
+            // iconUrl="code-pattern.svg"
+            showUserInfo={false}
+            enableTilt={true}
+            enableMobileTilt={false}
           />
-
-          <div
-            className="absolute bottom-0 left-0 w-full h-20
-             bg-gradient-to-t from-black/90 via-black/70 to-transparent
-             backdrop-blur-[2px]"
-          />
-
-          {/* Caption */}
-          <div className="absolute bottom-4 left-6 z-10">
-            <p className="text-xs text-gray-300 tracking-wide uppercase">
-              Plaza de Armas - Arequipa
-            </p>
-          </div>
         </div>
 
         <div className="flex flex-col justify-center gap-8 ">
